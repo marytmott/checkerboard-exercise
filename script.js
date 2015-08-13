a24cf32e708229af6af0aa48b2ca9cdd7d7eb940
Mary Mott <mott.maryt@gmail.com>
2015-08-12 23:01:52 -0700

    flashing

--- script.js
+++ script.js
@@ -1,40 +1,36 @@
 // Your JS goes here
 (function() {
-  var body = document.querySelector('body');
-  var redTile;
-  var blackTile;
-  //var size// = 63 tiles
-// Each tile should be a div
-// Each tile's width is 11.1%
-// Set each tile's float property to left
-// Each tile's paddingBottom is 11.1%
 
-// document.createElement('div'); //in a for loop?
-// redTile.style.backgroundColor = 'red';
-// redTile.style.width = '11.1%';
-// redTile.style.float = 'left';
-// redTile.style.paddingBottom = '11.1%';
+  function setTileColors() {
+    var body = document.querySelector('body');
+    var crazyTile;
+    var randomColor;
+    var r;
+    var g;
+    var b;
+    var color;
+    var divTiles;
 
-//create a bunch of divs
-  for (var i = 1; i <= 63; i++) {
-    if (i % 2 === 0) {
-      //red tiles
-    redTile = document.createElement('div');
-    redTile.style.backgroundColor = 'red';
-    redTile.style.width = '11.1%';
-    redTile.style.float = 'left';
-    redTile.style.paddingBottom = '11.1%';
-    body.appendChild(redTile);
+    document.body.innerHTML = '';
 
-    } else {
-      //black tiles
-      blackTile = document.createElement('div');
-      blackTile.style.backgroundColor = 'black';
-      blackTile.style.width = '11.1%';
-      blackTile.style.float = 'left';
-      blackTile.style.paddingBottom = '11.1%';
-      body.appendChild(blackTile);
+    //create a bunch of divs
+    for (var i = 1; i <= 63; i++) {
+      //populate with tiles
+      r = Math.floor(Math.random() * 255 + 1);
+      g = Math.floor(Math.random() * 255 + 1);
+      b = Math.floor(Math.random() * 255 + 1);
+      color = 'rgb(' + r + ',' + g + ',' + b + ')';
+
+      crazyTile = document.createElement('div');
+      crazyTile.style.backgroundColor = color;
+      crazyTile.style.width = '11.1%';
+      crazyTile.style.float = 'left';
+      crazyTile.style.paddingBottom = '11.1%';
+      body.appendChild(crazyTile);
     }
   }
 
+  setTileColors();
+  setInterval(setTileColors, 2000);
 })();
+
