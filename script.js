5c0b3c8ec62abc1f8efeda7ca93f3eee01ea8089
Mary Mott <mott.maryt@gmail.com>
2015-08-13 09:23:27 -0700

    basic board to update

--- script.js
+++ script.js
@@ -1,36 +1,29 @@
 // Your JS goes here
 (function() {
+  var body = document.querySelector('body');
+  var redTile;
+  var blackTile;
 
-  function setTileColors() {
-    var body = document.querySelector('body');
-    var crazyTile;
-    var randomColor;
-    var r;
-    var g;
-    var b;
-    var color;
-    var divTiles;
+  //create a bunch of divs
+  for (var i = 1; i <= 63; i++) {
+    if (i % 2 === 0) {
+    //red tiles
+    redTile = document.createElement('div');
+    redTile.style.backgroundColor = 'red';
+    redTile.style.width = '11.1%';
+    redTile.style.float = 'left';
+    redTile.style.paddingBottom = '11.1%';
+    body.appendChild(redTile);
 
-    document.body.innerHTML = '';
-
-    //create a bunch of divs
-    for (var i = 1; i <= 63; i++) {
-      //populate with tiles
-      r = Math.floor(Math.random() * 255 + 1);
-      g = Math.floor(Math.random() * 255 + 1);
-      b = Math.floor(Math.random() * 255 + 1);
-      color = 'rgb(' + r + ',' + g + ',' + b + ')';
-
-      crazyTile = document.createElement('div');
-      crazyTile.style.backgroundColor = color;
-      crazyTile.style.width = '11.1%';
-      crazyTile.style.float = 'left';
-      crazyTile.style.paddingBottom = '11.1%';
-      body.appendChild(crazyTile);
+    } else {
+      //black tiles
+      blackTile = document.createElement('div');
+      blackTile.style.backgroundColor = 'black';
+      blackTile.style.width = '11.1%';
+      blackTile.style.float = 'left';
+      blackTile.style.paddingBottom = '11.1%';
+      body.appendChild(blackTile);
     }
   }
 
-  setTileColors();
-  setInterval(setTileColors, 2000);
 })();
-
