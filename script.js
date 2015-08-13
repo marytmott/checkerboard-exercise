c3cbee6e8d74cde0da1890244cbe133982116783
Mary Mott <mott.maryt@gmail.com>
2015-08-12 23:14:06 -0700

    checkerboard cleaned up comments in script

--- script.js
+++ script.js
@@ -3,19 +3,8 @@
   var body = document.querySelector('body');
   var redTile;
   var blackTile;
-  //var size// = 63 tiles
-// Each tile should be a div
-// Each tile's width is 11.1%
-// Set each tile's float property to left
-// Each tile's paddingBottom is 11.1%
 
-// document.createElement('div'); //in a for loop?
-// redTile.style.backgroundColor = 'red';
-// redTile.style.width = '11.1%';
-// redTile.style.float = 'left';
-// redTile.style.paddingBottom = '11.1%';
-
-//create a bunch of divs
+  //create a bunch of divs
   for (var i = 1; i <= 63; i++) {
     if (i % 2 === 0) {
       //red tiles
