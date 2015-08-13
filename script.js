dff3c15a350312c6003abfa8721eb879b729f2f6
Mary Mott <mott.maryt@gmail.com>
2015-08-13 10:16:56 -0700

    work on gradient - doesn't work

--- script.js
+++ script.js
@@ -1,29 +1,48 @@
 // Your JS goes here
 (function() {
   var body = document.querySelector('body');
-  var magentaTile;
-  var cyanTile;
+  var gradientTile;
+  var r = 51;
+  var g = 65;
+  var b = 251;
+  var color;
 
   //create a bunch of divs
   for (var i = 1; i <= 63; i++) {
-    if (i % 2 === 0) {
-      //red tiles
-      magentaTile = document.createElement('div');
-      magentaTile.style.backgroundColor = 'red';
-      magentaTile.style.width = '11.1%';
-      magentaTile.style.float = 'left';
-      magentaTile.style.paddingBottom = '11.1%';
-      body.appendChild(redTile);
+    r += 10;
+    g += 10;
+    b += 10;
+    color = 'rgb(' + r + ',' + g + ',' + b + ')';
 
-    } else {
-      //black tiles
-      cyanTile = document.createElement('div');
-      cyanTile.style.backgroundColor = 'cyan'; //rgba(57, 194, 228, 1)
-      cyanTile.style.width = '11.1%';
-      cyanTile.style.float = 'left';
-      cyanTile.style.paddingBottom = '11.1%';
-      body.appendChild(blackTile);
-    }
+    gradientTile = document.createElement('div');
+    gradientTile.style.backgroundImage = color; //rgba(57, 194, 228, 1) - summer sky
+    gradientTile.style.width = '11.1%';
+    gradientTile.style.float = 'left';
+    gradientTile.style.paddingBottom = '11.1%';
+    body.appendChild(gradientTile);
+
+
+
+    // if (i % 2 === 0) {
+    //   //cyan tiles
+    //   cyanTile = document.createElement('div');
+    //   cyanTile.style.backgroundImage = 'cyan'; //rgba(57, 194, 228, 1) - summer sky
+    //   cyanTile.style.width = '11.1%';
+    //   cyanTile.style.float = 'left';
+    //   cyanTile.style.paddingBottom = '11.1%';
+    //   body.appendChild(cyanTile);
+
+    // } else {
+    //   //magenta
+    //   magentaTile = document.createElement('div');
+    //   magentaTile.style.backgroundImage = 'linear-gradient('; //rgba(192, 35, 227, 1) - veronica
+    //   magentaTile.style.width = '11.1%';
+    //   magentaTile.style.float = 'left';
+    //   magentaTile.style.paddingBottom = '11.1%';
+    //   body.appendChild(magentaTile);
+    // }
   }
 
 })();
+
+//RGBA(51, 65, 251, 1)
