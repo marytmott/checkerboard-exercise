30ab6441a95ac13f272f72096dc0d6093c77423d
Mary Mott <mott.maryt@gmail.com>
2015-08-13 09:27:47 -0700

    some colors and vars done

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 // Your JS goes here
 (function() {
   var body = document.querySelector('body');
-  var redTile;
-  var blackTile;
+  var magentaTile;
+  var cyanTile;
 
   //create a bunch of divs
   for (var i = 1; i <= 63; i++) {
     if (i % 2 === 0) {
-    //red tiles
-    redTile = document.createElement('div');
-    redTile.style.backgroundColor = 'red';
-    redTile.style.width = '11.1%';
-    redTile.style.float = 'left';
-    redTile.style.paddingBottom = '11.1%';
-    body.appendChild(redTile);
+      //red tiles
+      magentaTile = document.createElement('div');
+      magentaTile.style.backgroundColor = 'red';
+      magentaTile.style.width = '11.1%';
+      magentaTile.style.float = 'left';
+      magentaTile.style.paddingBottom = '11.1%';
+      body.appendChild(redTile);
 
     } else {
       //black tiles
-      blackTile = document.createElement('div');
-      blackTile.style.backgroundColor = 'black';
-      blackTile.style.width = '11.1%';
-      blackTile.style.float = 'left';
-      blackTile.style.paddingBottom = '11.1%';
+      cyanTile = document.createElement('div');
+      cyanTile.style.backgroundColor = 'cyan'; //rgba(57, 194, 228, 1)
+      cyanTile.style.width = '11.1%';
+      cyanTile.style.float = 'left';
+      cyanTile.style.paddingBottom = '11.1%';
       body.appendChild(blackTile);
     }
   }
